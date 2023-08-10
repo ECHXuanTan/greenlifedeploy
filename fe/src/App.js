4cf80028b776ae3a541c1b5ef8d06d71903ffc5c
ECHXuanTan <93677722+ECHXuanTan@users.noreply.github.com>
2023-08-11 02:48:42 +0700

    update 12

--- fe/src/App.js
+++ fe/src/App.js
@@ -49,7 +49,7 @@ function App() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const { data } = await axios.get(`/api/products/categories`);
+        const { data } = await axios.get(`https://greenlife-deploy-5.onrender.com/api/products/categories`);
         setCategories(data);
       } catch (err) {
         toast.error(getError(err));
